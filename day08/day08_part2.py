cb7ed0a47827208c39fc668a04b87339a64ce6c3
mathieu <mathieu@pounains.homeip.net>
2018-12-20 16:36:17 +0100

    day 8 part 2

--- day08/day08_part2.py
+++ day08/day08_part2.py
@@ -8,8 +8,6 @@ from collections import defaultdict
 with open("input") as f:
     content = f.read().replace('\n','').split(' ')
 
-print(content)
-
 def read_node(elements):
 #    print("Read node from %s" % (elements))
     nb_child_nodes = int(elements[0])
@@ -33,13 +31,17 @@ def read_node(elements):
     return dict(childs= childs, metadata= metadata), size
 
 def sum_metadata(node):
-    total_metadata = reduce((lambda x, y: x + y), node['metadata'])
-    total_childs = 0
+    if len(node['childs']) == 0:
+        # no child, count only metadata
+        return reduce((lambda x, y: x + y), node['metadata'])
 
-    if len(node['childs']) > 0:
-        total_childs = reduce((lambda x, y: x + y), list(map(sum_metadata, node['childs'])))
+    total = 0
+    # process childs
+    for n in node['metadata']:
+        if n > 0 and n <= len(node['childs']):
+            total += sum_metadata(node['childs'][n-1])
 
-    return total_metadata + total_childs
+    return total
 
 root, total_size = read_node(content)
 print("root = %s" % (root))
